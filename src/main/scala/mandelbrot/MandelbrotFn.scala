f0cd0d69051d2fb0157aefbb49e0567d6d619409
Ben Grant <benjamin.j.grant@gmail.com>
2022-03-07 01:23:17 -0800

    Change MandelbrotFn to accept enable input and precision parameter

--- src/main/scala/mandelbrot/MandelbrotFn.scala
+++ src/main/scala/mandelbrot/MandelbrotFn.scala
@@ -30,17 +30,24 @@ object Complex {
 
 /**
   * Compute z^2 + c on fixed-point imaginary numbers
+  * 
+  * @param precision number of bits after the binary point
   */
-class MandelbrotFn(width: Int, binaryPoint: Int) extends Module {
+class MandelbrotFn(precision: Int) extends Module {
 	val io = IO(new Bundle {
-		val z = Input(new Complex(width, binaryPoint))
-		val c = Input(new Complex(width, binaryPoint))
-		val out = Output(new Complex(width, binaryPoint))
+		val z      = Input(Complex(precision))
+		val c      = Input(Complex(precision))
+		val enable = Input(Bool())
+		val out    = Output(Complex(precision))
 	})
 
-	// let z = AZ + BZi, c = AC + BCi
-	// z^2 = (AZ + BZi)^2 = AZ^2 + (2AZBZ)i + (BZi)^2
-	//                    = (AZ^2 - BZ^2) + (2AZBZ)i
-	io.out.re := (io.z.re * io.z.re) - (io.z.im * io.z.im) + io.c.re
-	io.out.im := ((io.z.re * io.z.im) << 1) + io.c.im
+	when(io.enable) {
+		// let z = AZ + BZi, c = AC + BCi
+		// z^2 = (AZ + BZi)^2 = AZ^2 + (2AZBZ)i + (BZi)^2
+		//                    = (AZ^2 - BZ^2) + (2AZBZ)i
+		io.out.re := (io.z.re * io.z.re) - (io.z.im * io.z.im) + io.c.re
+		io.out.im := ((io.z.re * io.z.im) << 1) + io.c.im
+	}.otherwise {
+		io.out := DontCare
+	}
 }
--- src/test/scala/mandelbrot/MandelbrotTester.scala
+++ src/test/scala/mandelbrot/MandelbrotTester.scala
@@ -7,14 +7,15 @@ import org.scalatest.flatspec.AnyFlatSpec
 class MandelbrotTester extends AnyFlatSpec with ChiselScalatestTester {
 	behavior of "MandelbrotFn"
 	it should "calculate z^2 + c correctly" in {
-		test(new MandelbrotFn(16, 8)) { dut =>
-			dut.io.z.poke(Complex(16, 8, 3.0, -2.0))
-			dut.io.c.poke(Complex(16, 8, 6.0, -8.0))
-			dut.io.out.expect(Complex(16, 8, 11.0, -20.0))
+		test(new MandelbrotFn(16)) { dut =>
+			dut.io.enable.poke(true.B)
+			dut.io.z.poke(Complex(20, 16, 0.375, 0.46875))
+			dut.io.c.poke(Complex(20, 16, -0.875, 0.1875))
+			dut.io.out.expect(Complex(20, 16, -0.9541015625, 0.5390625))
 
-			dut.io.z.poke(Complex(16, 8, 0.125, -0.1875))
-			dut.io.c.poke(Complex(16, 8, 0.3125, 0.75))
-			dut.io.out.expect(Complex(16, 8, 0.29296875, 0.703125))
+			dut.io.z.poke(Complex(20, 16, 0.125, -0.1875))
+			dut.io.c.poke(Complex(20, 16, 0.3125, 0.75))
+			dut.io.out.expect(Complex(20, 16, 0.29296875, 0.703125))
 		}
 	}
 }
