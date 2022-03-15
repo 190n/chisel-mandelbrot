a486f1f632d5570d4bfa4a816dec8c941c8a1de7
Ben Grant <benjamin.j.grant@gmail.com>
2022-03-14 20:40:59 -0700

    Use 32-bit precision in MandelbrotRunner

--- src/test/scala/mandelbrot/MandelbrotRunner.scala
+++ src/test/scala/mandelbrot/MandelbrotRunner.scala
@@ -8,7 +8,7 @@ import scala.collection.mutable.ArrayBuffer
 class MandelbrotRunner extends AnyFlatSpec with ChiselScalatestTester {
 	behavior of "Mandelbrot"
 	it should "run with arguments" in {
-		val p = new MandelbrotParams(3, 30, 5, 100)
+		val p = new MandelbrotParams(32, 30, 5, 100, 0.125)
 		test(new Mandelbrot(p)) { dut =>
 			val cyclesPerCell = dut.p.iters + 2
 			val numComputeBlocks = (dut.p.rows * dut.p.cols) / dut.p.parallelism
@@ -19,10 +19,17 @@ class MandelbrotRunner extends AnyFlatSpec with ChiselScalatestTester {
 				dut.clock.step()
 			}
 
-			// A row (split into its blocks) should be
+			// A row (split into its blocks) should be output in the order
 			// [-2.0, -1.5) [-1.5, -1.0) [-1.0, -0.5) [-0.5, 0.0) [0.0, 0.5)
-			// but it is actually
+			// but it is actually output in the order
 			// [0.0, 0.5) [-2.0, -1.5) [-1.5, -1.0) [-1.0, -0.5) [-0.5, 0.0)
+			// Sadly, I'm not sure why. I've checked where the results are stored in Mandelbrot's
+			// memory, and where the results are retrieved from that memory to be output, and
+			// nothing seems amiss. That would suggest that somehow the MnadelbrotIter modules are
+			// receiving the wrong input, but I have checked that too, as well as the behavior of
+			// MandelbrotIter itself.
+			// Anyway, this function takes an index that counts up from zero and returns where to
+			// look in the array to find that index.
 			def fixStartIndex(i: Int) = {
 				if (i < p.elementsPerTransfer) {
 					i + p.cols - p.elementsPerTransfer
